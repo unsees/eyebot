4854fd3749ea783b4b39e9b3a57689dfc2f4e166
unsees <78049013+unsees@users.noreply.github.com>
2021-01-26 16:51:06 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('YOU', { type: 'WATCHING' });
+    client.user.setActivity('you', { type: 'WATCHING' });
 });
 
 client.on('guildMemberAdd', member => {
